ed36fac846ad50a711ae74e81d10cfa6d69347fc
niclin <bboyceo@hotmail.com>
2016-04-14 15:52:18 +0800

    add Readme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414063254) do
+ActiveRecord::Schema.define(version: 20160414064857) do
 
   create_table "coaches", force: :cascade do |t|
     t.string   "name"
