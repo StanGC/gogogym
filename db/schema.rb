c76103d724ea8516965ed6a6aae8eb76ba6ced92
Stan <solve153@gmail.com>
2016-04-14 22:34:29 +0800

    Gem install for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20160414124807) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "partners", force: :cascade do |t|
+    t.string   "name"
+    t.text     "introduction"
+    t.string   "haunt_time"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "photos", force: :cascade do |t|
     t.integer  "gym_id"
     t.string   "image"
