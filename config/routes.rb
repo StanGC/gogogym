04ff3994b9b2f6bbd185790734a471d9cb797bd8
Stan <solve153@gmail.com>
2016-04-15 11:08:14 +0800

    finish backstage

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
       end
     end
     resources :coaches
+    resources :gyms
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
