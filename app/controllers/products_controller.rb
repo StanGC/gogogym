724e94631ae20817bab8caedae088a105275ec2b
Stan <solve153@gmail.com>
2016-04-15 11:24:36 +0800

    Add shopping cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @products = Product.all
   end
@@ -13,5 +13,16 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path, alert: "商品已下架"
+  end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    if !current_cart.items.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
+    else
+      flash[:warning] = "你的購物車內已有此物品"
+    end
+    redirect_to :back
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414124807) do
+ActiveRecord::Schema.define(version: 20160415032025) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "coach_schedules", force: :cascade do |t|
     t.date     "date"
